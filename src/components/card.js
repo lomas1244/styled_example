9e82a444184fcf0b3c7c24e92d39efb88a834625
James Lomas <jameslomas2004@gmail.com>
2019-08-06 15:17:31 +0100

    testing with styled-theming

--- src/components/card.js
+++ src/components/card.js
@@ -1,33 +1,34 @@
 import React from "react"
 import styled from 'styled-components'
 
-/* Declare constants for the named elements and declare HTML element type after 'styled.' */
-const CardContainer = styled.div`
-padding: 30px;
-border-radius: 10px;
-box-shadow: 0px 0px 15px rgba(0,0,0,0.15);
-width: 300px;
-margin: 100px auto;
-text-align: left;
-`
+// Define our button, but with the use of props.theme this time
+const Button = styled.button`
+  color: ${props => props.theme.fg};
+  border: 2px solid ${props => props.theme.fg};
+  background: ${props => props.theme.bg};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+`;
+// Define our `fg` and `bg` on the theme
+const buttonStyle = {
+  fg: "palevioletred",
+  bg: "white"
+};
+// This theme swaps `fg` and `bg`
+const invertTheme = ({ fg, bg }) => ({
+  fg: bg,
+  bg: fg
+});
 
-const CardTitle = styled.h3`
-font-size: 40px;
-color: #e7e7e7;
-margin-bottom: 0;
-margin-top: 10px;
-`
-
-const CardDesc = styled.p`
-font-size: 18px;
-margin-top: 10px;
-`
-
-const Card = () => (
-  <CardContainer>
-    <CardTitle>Card title</CardTitle>
-    <CardDesc>This is the text which gives meaning to the card</CardDesc>
-  </CardContainer>
-)
-
-export default Card
+render(
+<ThemeProvider theme={theme}>
+    <div>
+      <Button>Default Theme</Button>
+      <ThemeProvider theme={invertTheme}>
+        <Button>Inverted Theme</Button>
+      </ThemeProvider>
+    </div>
+  </ThemeProvider>
+);
